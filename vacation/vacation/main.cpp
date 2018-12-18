bee14a6cc9c462e5c6035d7b625757061b0934d9
이명희 <imyeonghui@imyeonghuiui-macbook-pro.local>
2018-12-18 21:01:19 +0900

    test

---
+++ vacation/vacation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vacation
+//
+//  Created by 이명희 on 2018. 12. 18..
+//  Copyright © 2018년 이명희. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
